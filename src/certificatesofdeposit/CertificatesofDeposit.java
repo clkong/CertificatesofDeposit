b4cfb35687ffa07246bd4f1d81a4e6314a561a27
clkong <36968245+clkong@users.noreply.github.com>
2018-04-03 13:59:27 -0400

    Update CertificatesofDeposit.java

--- src/certificatesofdeposit/CertificatesofDeposit.java
+++ src/certificatesofdeposit/CertificatesofDeposit.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Sean Luo 03/04/2018
+ * CertificatesOfDeposit.java
+ * This program calculates the CertificatesOfDeposit
  */
 
 package certificatesofdeposit;
